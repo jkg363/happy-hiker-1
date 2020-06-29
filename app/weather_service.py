fd5df08d0b43405facc2f727d73c4d580c16d519
jkg363 <65503702+jkg363@users.noreply.github.com>
2020-06-29 19:27:16 -0400

    Pulled Zip Code from Park Selection

--- app/weather_service.py
+++ app/weather_service.py
@@ -1,61 +1,85 @@
 # app/weather_service.py
 
 import os
-import pandas as pd
+import csv
 import json
 from pprint import pprint
 
-import requests
-from dotenv import load_dotenv
+#import requests
+#from dotenv import load_dotenv
 
-from app import APP_ENV
+#from app import APP_ENV
 
-parks_df = pd.read_csv("Trail Park Database - Parks.csv")
+csv_filepath = os.path.join(os.path.dirname(__file__), "Trail Park Database - Parks.csv")
 
-#print(parks_df["park"])
+with open(csv_filepath, "r") as csv_file:
+    parks = []
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        parks.append(row)
 
-load_dotenv()
+destination = []
+while True:
+    park_id = input("Hey Happy Hiker! Please Input Your Park: ")
 
-OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
-MY_ZIP = os.getenv("MY_ZIP", default="20057")
-COUNTRY_CODE = os.getenv("COUNTRY_CODE", default="US")
-
-def human_friendly_temp(my_temperature_f):
-    """Rounds a decimal fahrenheit temperature to the nearest whole degree, adds degree symbol"""
-    degree_sign = u"\N{DEGREE SIGN}"
-    return f"{round(my_temperature_f)} {degree_sign}F"
-
-def get_hourly_forecasts(zip_code=MY_ZIP, country_code=COUNTRY_CODE):
-    # see: https://openweathermap.org/current
-    request_url = f"https://api.openweathermap.org/data/2.5/forecast?zip={zip_code},{country_code}&units=imperial&appid={OPEN_WEATHER_API_KEY}"
-    response = requests.get(request_url)
-    parsed_response = json.loads(response.text)
-    #print(parsed_response.keys()) #> dict_keys(['cod', 'message', 'cnt', 'list', 'city'])
-    result = {
-        "city_name": parsed_response["city"]["name"],
-        "hourly_forecasts": []
-    }
-    for forecast in parsed_response["list"][0:9]:
-        #print(forecast.keys()) #> dict_keys(['dt', 'main', 'weather', 'clouds', 'wind', 'sys', 'dt_txt'])
-        result["hourly_forecasts"].append({
-            "timestamp": forecast["dt_txt"],
-            "temp": human_friendly_temp(forecast["main"]["feels_like"]),
-            "conditions": forecast["weather"][0]["description"]
-        })
-    return result
-
-if __name__ == "__main__":
-
-    if APP_ENV == "development":
-        park_name = input("HEY HAPPY HIKER! PLEASE SELECT YOUR PARK (e.g. Bear Mountain State Park): ")
-
-        results = get_hourly_forecasts(zip_code=zip_code) # invoke with custom params
+    if [p for p in parks if str(p["park"]) == park_id]:
+        destination.append(park_id)
+        break
     else:
-        results = get_hourly_forecasts() # invoke with default params
+        print("We don't play there! Please Re-Enter.")
 
-    print("-----------------")
-    print(f"TODAY'S WEATHER FORECAST FOR {results['city_name'].upper()}...")
-    print("-----------------")
+for park_id in destination:
+    matching_locations = [i for i in parks if str(i["park"]) == str(park_id)]
+    matching_location = matching_locations[0]
+    for i in matching_locations:
+        zip_code = i["zipcode"]
 
-    for hourly in results["hourly_forecasts"]:
-        print(hourly["timestamp"], "|", hourly["temp"], "|", hourly["conditions"])
+
+
+
+#load_dotenv()
+#
+#OPEN_WEATHER_API_KEY = os.getenv("OPEN_WEATHER_API_KEY")
+#MY_ZIP = os.getenv("MY_ZIP", default="20057")
+#COUNTRY_CODE = os.getenv("COUNTRY_CODE", default="US")
+#
+#def human_friendly_temp(my_temperature_f):
+#    """Rounds a decimal fahrenheit temperature to the nearest whole degree, adds degree symbol"""
+#    degree_sign = u"\N{DEGREE SIGN}"
+#    return f"{round(my_temperature_f)} {degree_sign}F"
+#
+#def get_hourly_forecasts(zip_code=MY_ZIP, country_code=COUNTRY_CODE):
+#    # see: https://openweathermap.org/current
+#    request_url = f"https://api.openweathermap.org/data/2.5/forecast?zip={zip_code},{country_code}&units=imperial&appid={OPEN_WEATHER_API_KEY}"
+#    response = requests.get(request_url)
+#    parsed_response = json.loads(response.text)
+#    #print(parsed_response.keys()) #> dict_keys(['cod', 'message', 'cnt', 'list', 'city'])
+#    result = {
+#        "city_name": parsed_response["city"]["name"],
+#        "hourly_forecasts": []
+#    }
+#    for forecast in parsed_response["list"][0:9]:
+#        #print(forecast.keys()) #> dict_keys(['dt', 'main', 'weather', 'clouds', 'wind', 'sys', 'dt_txt'])
+#        result["hourly_forecasts"].append({
+#            "timestamp": forecast["dt_txt"],
+#            "temp": human_friendly_temp(forecast["main"]["feels_like"]),
+#            "conditions": forecast["weather"][0]["description"]
+#        })
+#    return result
+#
+#if __name__ == "__main__":
+#
+#    if APP_ENV == "development":
+#        park_name = input("HEY HAPPY HIKER! PLEASE SELECT YOUR PARK (e.g. Bear Mountain State Park): ")
+#
+#        results = get_hourly_forecasts(zip_code=zip_code) # invoke with custom params
+#    else:
+#        results = get_hourly_forecasts() # invoke with default params
+#
+#    print("-----------------")
+#    print(f"TODAY'S WEATHER FORECAST FOR {results['city_name'].upper()}...")
+#    print("-----------------")
+#
+#    for hourly in results["hourly_forecasts"]:
+#        print(hourly["timestamp"], "|", hourly["temp"], "|", hourly["conditions"])
+#
